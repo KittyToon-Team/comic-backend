cedafcedd86519947f7008139bc625b1ff1eda37
Quỳnh Em <anhptqth05962@gmail.com>
2026-07-03 23:15:08 +0700

    create project

---
+++ src/main/java/org/example/comicbackend/Controller/TestController.java
@@ -0,0 +1,15 @@
+package org.example.comicbackend.Controller; // Nhớ đổi tên package cho khớp máy bạn
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:5175"})
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testConnection() {
+        return "Xin chào! Kết nối giữa Vue 3 và Spring Boot đã thành công rực rỡ!";
+    }
+}
---
+++ src/test/java/org/example/comicbackend/ComicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.comicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
